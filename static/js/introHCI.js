b737a83ec47bb1a3a61468bbebbb2b537118a166
yac073 <yac073@ucsd.edu>
2017-01-19 10:18:44 -0800

    all done

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -24,5 +24,7 @@ function projectClick(e) { 
     e.preventDefault();
     // In an event handler, $(this) refers to      
     // the object that triggered the event      
-    $(this).css("background-color", "#7fff00");
+    var projectTitle = $(this).find("p").text();
+    var jumbotronHeader = $(".jumbotron h1");
+    jumbotronHeader.text(projectTitle);
 }
